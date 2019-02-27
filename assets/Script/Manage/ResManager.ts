dcc4ddb9a3a40ab2fde76948052e38a25d39b941
leo <leo704373978@gmail.com>
2019-02-28 00:37:48 +0800

    添加ListView

--- assets/Script/Manage/ResManager.ts
+++ assets/Script/Manage/ResManager.ts
@@ -1,10 +1,10 @@
 
 
-export let resGetPrefab=function (url): Promise<cc.Prefab> {
+export let resGetPrefab = function (url: string): Promise<cc.Prefab> {
     console.log('getPrefab');
     let p = new Promise<cc.Prefab>((resolve, reject) => {
         cc.loader.loadRes(url, (e, prefab) => {
-            console.log('loadRes',url);
+            console.log('loadRes', url);
             if (e) {
                 reject(e);
             } else {
@@ -13,4 +13,22 @@ export let resGetPrefab=function (url): Promise<cc.Prefab> {
         });
     });
     return p;
+}
+
+export let loadImage = function (url: string, type?: string) {
+    console.log('loadImage');
+    let p = new Promise<cc.SpriteFrame>((resolve, reject) => {
+        console.log("load image ");
+        cc.loader.load({
+            url: url,
+            type: type ? type : "jpg"
+        }, (error, data) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            let spriteFrame = new cc.SpriteFrame(data, cc.rect(0, 0, data.width, data.height));
+            resolve(spriteFrame);
+        });
+    });
 }
