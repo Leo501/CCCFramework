3c5cbfd8eac0ded721ae5f13ef4b6d3f18236f18
Leo <907600065@qq.com>
2019-10-30 23:27:50 +0800

    添加Base文件夹

---
+++ assets/hall/Script/Base/BaseConfig.ts
@@ -0,0 +1,13 @@
+
+
+export default class BaseConfig {
+
+    public static proxyPorts = [];
+    public static initProxy = false;//高防
+    public static ignoreUpdate = true;   //忽略热更?
+    public static iosCheck = false;       //ios提审开关
+    public static iosCheckPak = false;     //ios提审包
+    public static winAutoLogin = false; //windows是否自动登录
+    public static dnsResolves = [0, 1];  //http访问时使用的dns预解析方式(0:不解析  1:httpdns ...)
+
+}
---
+++ assets/hall/Script/GameGonfig.ts
@@ -0,0 +1,25 @@
+import BaseConfig from "./Base/BaseConfig";
+
+export default class GameConfig extends BaseConfig {
+    public static checkUrls = {};
+    //备份下载地址
+    private static backupDownloadUrls = ['https://fivem.whxbwg.com'];
+
+    private static serverUrls = ['https://192.168.193.50:8331'];
+
+    private static safeUrls = ['https://192.168.193.50:8331'];
+    // 服务器类型 0正式服 1测试服 2本地
+    public static type = 2;
+
+    public static getBackupDownloadUrl(): string {
+        return this.backupDownloadUrls[this.type];
+    }
+
+    public static getServerUrl(): string {
+        return this.serverUrls[this.type];
+    }
+
+    public static getSafeUrl() {
+        return this.safeUrls[this.type];
+    }
+}
