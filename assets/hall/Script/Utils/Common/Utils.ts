a99aaf1cb52baef12c89addb413ebe05514ec4d5
leo <907600065@qq.com>
2020-03-13 17:27:20 +0800

    Uilts.random is int

--- assets/hall/Script/Utils/Common/Utils.ts
+++ assets/hall/Script/Utils/Common/Utils.ts
@@ -1,6 +1,7 @@
 export class Utils {
 
     public static random(mix: number, max: number) {
-        return mix + (max - mix) * Math.random();
+        let num = mix + (max - mix) * Math.random();
+        return Math.floor(num);
     }
 }
